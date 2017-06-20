b08caf7412251ebb4eb98e39ea63337c45fe3062
Brach Hadean <hadean.brach@gmail.com>
2017-06-20 11:14:18 -0600

    init commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FlashCardApp from './components/FlashCardsApp';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+          <h2>Welcome to Flash Cards</h2>
+        <FlashCardApp />
+      </div>
+    );
+  }
+}
+
+export default App;
